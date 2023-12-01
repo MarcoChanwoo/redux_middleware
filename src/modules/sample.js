531d1e2cd383431c2fbbd3532483359582303802
Marco <cwhong8087@gmail.com>
2023-12-02 00:35:33 +0900

    “updated”

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,3 +1,4 @@
+import * as api from "../lib/api";
 // 액션 타입 선언
 // 한 요청당 새 개를 만듦
 const GET_POST = "sample/GET_POST";
@@ -10,3 +11,21 @@ const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
 // thunk 함수 생성
 // thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치함
+
+export const getPost = (id) => async (dispatch) => {
+  dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림
+  try {
+    const response = await api.getPost(id);
+    dispatch({
+      type: GET_POST_SUCCESS,
+      payload: response.data,
+    }); // 요청 성공
+  } catch (e) {
+    dispatch({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true,
+    }); // 에러 발생
+    throw e; // 나중에 컴포넌트 단에서 에러를 조회할 수 있게 해 줌
+  }
+};
